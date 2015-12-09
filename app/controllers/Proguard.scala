12fd587dc56cb3f93a5c44d581b2891be639f11b
Dwite <dwite.lol@gmail.com>
2015-12-09 22:07:12 +0200

    Added proguard file reading and filtering

--- app/controllers/Proguard.scala
+++ app/controllers/Proguard.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import play.api.Play
 import play.api.Play.current
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 
@@ -12,19 +13,36 @@ import play.api.mvc.{Action, Controller}
   */
 class Proguard extends Controller {
 
-  def proguard(libraryName : String) = Action {
-    Ok(views.html.proguard(libraryName))
+  val proguardFolder = "/public/proguards/"
+  val proguardSuffix = "proguard-"
+  val proguardExtension = ".pro"
+
+  def proguard(libraryName: String) = Action {
+    val libraries = libraryName.split(',')
+    if (libraries.length > 0) {
+      val resultProguard = s"\n# Created by https://www.proguard.io/api/$libraryName\n"
+      Ok(resultProguard + "\n" + (listInDir(proguardFolder).filter(libraries.contains(_)).map(readFile) mkString "\n"))
+    } else {
+      Ok(views.html.proguard(""))
+    }
   }
 
   def list() = Action {
-    Ok(listInDir("/public/proguards"))
-  }
-  private def listInDir(filePath : String) : String = {
-    getListOfFiles(Play.getFile(filePath)).map(_.getName) mkString "\r\n"
+    Ok(Json.toJson(listInDir(proguardFolder)))
   }
 
-  private def getListOfFiles(dir: File) : List[File] = {
+  private def listInDir(filePath: String): List[String] = {
+    getListOfFiles(Play.getFile(filePath)).map(_.getName.replace(proguardExtension, "").replace(proguardSuffix, ""))
+  }
+
+  def getListOfFiles(dir: File): List[File] = {
     dir.listFiles.filter(_.isFile).toList
+  }
+
+  def readFile(string: String): String = {
+    val source = scala.io.Source.fromFile(Play.getFile(s"$proguardFolder$proguardSuffix$string$proguardExtension"))
+    val lines = try source.mkString finally source.close()
+    lines
   }
 
 }
