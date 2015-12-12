2dd5a93fa8041c8fb3bd7e4790de635cc59845fd
Dwite <dwite.lol@gmail.com>
2015-12-12 22:07:46 +0200

    JS fix

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,53 @@
-'use strict';$(document).ready(function(){$.ajax('https://www.gitignore.io/dropdown/templates.json').success(function(data){$("#ignoreSearch").select2({sorter:function(results){var query=$('.select2-search__field').val().toLowerCase();return results.sort(function(a,b){return a.text.toLowerCase().indexOf(query)-
-b.text.toLowerCase().indexOf(query);});},placeholder:"Search library names",multiple:true,minimumInputLength:1,data:data});$("#ignoreSearch").select2("container").find("ul.select2-choices").sortable({containment:'parent',start:function(){$("#ignoreSearch").select2("onSortStart");},update:function(){$("#ignoreSearch").select2("onSortEnd");}});});});function generateGitIgnore(){var searchString=$("#ignoreSearch").val();var searchLength=searchString.length;if(searchLength>0){var files=searchString;var uriEncodedFiles=encodeURIComponent(files);window.location="/api/"+uriEncodedFiles;$("#ignoreSearch").val("");}}
-function generateGitIgnoreFile(){var searchString=$("#ignoreSearch").val();var searchLength=searchString.length;if(searchLength>0){var files=searchString;var uriEncodedFiles=encodeURIComponent(files);window.location="/api/f/"+uriEncodedFiles;}}
+var data = [{
+    id: 0,
+    text: 'enhancement'
+}, {
+    id: 1,
+    text: 'bug'
+}, {
+    id: 2,
+    text: 'duplicate'
+}, {
+    id: 3,
+    text: 'invalid'
+}, {
+    id: 4,
+    text: 'wontfix'
+}];
+
+'use strict';
+$(document).ready(function() {
+    $("#ignoreSearch").select2({
+        sorter: function(results) {
+            var query = $('.select2-search__field').val().toLowerCase();
+            return results.sort(function(a, b) {
+                return a.text.toLowerCase().indexOf(query) -
+                    b.text.toLowerCase().indexOf(query);
+            });
+        },
+        placeholder: "Search library names",
+        multiple: true,
+        minimumInputLength: 1,
+        data: data
+    }); $("#ignoreSearch").select2("container").find("ul.select2-choices").sortable({
+        containment: 'parent',
+        start: function() {
+            $("#ignoreSearch").select2("onSortStart");
+        },
+        update: function() {
+            $("#ignoreSearch").select2("onSortEnd");
+        }
+    });
+})
+
+
+function generateGitIgnore() {
+    var searchString = $("#ignoreSearch").val();
+    var searchLength = searchString.length;
+    if (searchLength > 0) {
+        var files = searchString;
+        var uriEncodedFiles = encodeURIComponent(files);
+        window.location = "/api/" + uriEncodedFiles;
+        $("#ignoreSearch").val("");
+    }
+}
